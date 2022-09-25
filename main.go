b397f1fa40a04981cd2f9aa8a1f5f512d9310fdd
李文周 <liwenzhou7@gmail.com>
2022-09-25 16:05:40 +0800

    feat: HTTP API and gRPC API in one port

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
 	helloworldpb "github.com/Q1mi/greeter/proto/helloworld"
-
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime" // 注意v2版本
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -27,7 +29,7 @@ func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*
 
 func main() {
 	// Create a listener on TCP port
-	lis, err := net.Listen("tcp", ":8080")
+	lis, err := net.Listen("tcp", ":8091")
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
@@ -36,36 +38,34 @@ func main() {
 	s := grpc.NewServer()
 	// 注册Greeter service到server
 	helloworldpb.RegisterGreeterServer(s, &server{})
-	// 8080端口启动gRPC Server
-	log.Println("Serving gRPC on 0.0.0.0:8080")
-	go func() {
-		log.Fatalln(s.Serve(lis))
-	}()
 
-	// 创建一个连接到我们刚刚启动的 gRPC 服务器的客户端连接
-	// gRPC-Gateway 就是通过它来代理请求（将HTTP请求转为RPC请求）
-	conn, err := grpc.DialContext(
-		context.Background(),
-		"0.0.0.0:8080",
-		grpc.WithBlock(),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		log.Fatalln("Failed to dial server:", err)
-	}
-
+	// gRPC-Gateway mux
 	gwmux := runtime.NewServeMux()
-	// 注册Greeter
-	err = helloworldpb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	dops := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err = helloworldpb.RegisterGreeterHandlerFromEndpoint(context.Background(), gwmux, "127.0.0.1:8091", dops)
 	if err != nil {
-		log.Fatalln("Failed to register gateway:", err)
+		log.Fatalln("Failed to register gwmux:", err)
 	}
 
+	mux := http.NewServeMux()
+	mux.Handle("/", gwmux)
+
+	// 定义HTTP server配置
 	gwServer := &http.Server{
-		Addr:    ":8090",
-		Handler: gwmux,
+		Addr:    "127.0.0.1:8091",
+		Handler: grpcHandlerFunc(s, mux), // 请求的统一入口
 	}
-	// 8090端口提供gRPC-Gateway服务
-	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
-	log.Fatalln(gwServer.ListenAndServe())
+	log.Println("Serving on http://127.0.0.1:8091")
+	log.Fatalln(gwServer.Serve(lis)) // 启动HTTP服务
+}
+
+// grpcHandlerFunc 将gRPC请求和HTTP请求分别调用不同的handler处理
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
